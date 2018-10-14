8b920f74f5f67501a3233914d95b6af6f4032eba
Brian Litwin <brian.n.litwin@gmail.com>
2018-10-14 19:18:56 -0400

    updates

--- Classes/View Controllers/UIViewController+SearchIssuesForLabel.swift
+++ Classes/View Controllers/UIViewController+SearchIssuesForLabel.swift
@@ -11,12 +11,23 @@ import UIKit
 extension UIViewController {
 
     func searchForLabelInRepositoryIssuesViewController(label: String) {
-        print(label)
-        if let o = self as? IssuesViewController {
-            o.dismiss?(label)
+        if let issuesViewController = self as? IssuesViewController {
+            guard let navController = issuesViewController.navigationController,
+                navController.viewControllers.count > 1,
+                let repositoryViewController = navController
+                    .viewControllers[1] as? RepositoryViewController
+                else { return }
+            
+//            repositoryViewController
+//                .searchForLabelInRepositoryIssuesViewController(
+//                label: label
+//            )
+            
+            navController.viewControllers.popLast()
+            
+            
             // + scroll to top 
         }
     }
-    
 }
 
