9dae8fd5c04345a7890b429cbfb4d31c4b355fbc
Brian Litwin <brian.n.litwin@gmail.com>
2018-10-14 18:39:25 -0400

    working draft

---
+++ Classes/View Controllers/UIViewController+SearchIssuesForLabel.swift
@@ -0,0 +1,30 @@
+//
+//  UIViewController+SearchIssuesForLabel.swift
+//  Freetime
+//
+//  Created by B_Litwin on 10/14/18.
+//  Copyright © 2018 Ryan Nystrom. All rights reserved.
+//
+
+import UIKit
+
+class UIViewController_SearchIssuesForLabel: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
