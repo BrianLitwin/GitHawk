3566dbe68134f6f53b8d1957e41368af3249a05e
Brian Litwin <brian.n.litwin@gmail.com>
2018-10-21 10:19:45 -0400

    first pass

--- Classes/Issues/Title/IssueTitleSectionController.swift
+++ Classes/Issues/Title/IssueTitleSectionController.swift
@@ -9,12 +9,13 @@
 import UIKit
 import IGListKit
 import GitHubAPI
+import ContextMenu
 
 protocol IssueTitleSectionControllerDelegate: class {
     func sendEditTitleRequest(title: String)
 }
 
-final class IssueTitleSectionController: ListSectionController {
+final class IssueTitleSectionController: ListSectionController, ContextMenuDelegate {
 
     var object: IssueTitleModel?
     weak var delegate: IssueTitleSectionControllerDelegate?
@@ -46,8 +47,99 @@ final class IssueTitleSectionController: ListSectionController {
     
     override func didSelectItem(at index: Int) {
         guard let object = self.object else { return }
-        let title = "Update Date in Workout History after changing it in workout"
-        delegate?.sendEditTitleRequest(title: title)
+        editIssueTitle(title: object.string.string.allText)
+//        let title = "Update Date in Workout History after changing it in workout"
+//        delegate?.sendEditTitleRequest(title: title)
+    }
+    
+    func editIssueTitle(title: String) {
+        guard let sourceViewController = self.viewController else { return }
+        let viewController = EditIssueTitleViewController(title: title)
+        ContextMenu.shared.show(
+            sourceViewController: sourceViewController,
+            viewController: viewController,
+            options: ContextMenu.Options(
+                containerStyle: ContextMenu.ContainerStyle(
+                    backgroundColor: Styles.Colors.menuBackgroundColor.color
+                )
+            )
+        )
     }
 
+    // MARK: ContextMenuDelegate
+    
+    func contextMenuWillDismiss(viewController: UIViewController, animated: Bool) {
+        guard let viewController = viewController as? EditIssueTitleViewController else {
+            return
+        }
+        
+        
+    }
+    
+    func contextMenuDidDismiss(viewController: UIViewController, animated: Bool) { }
+    
+}
+
+// issueManagingSectionController
+// save spins during network request
+
+
+class EditIssueTitleViewController: UIViewController {
+    
+    let originalTitle: String
+    
+    init(title: String) {
+        self.originalTitle = title
+        super.init(nibName: nil, bundle: nil)
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let textView = UITextView()
+        view.addSubview(textView)
+        textView.text = title
+        let font = Styles.Text.body.preferredFont
+        textView.font = font
+        let textSize = originalTitle.size(font: font)
+        textView.frame = CGRect(
+            x: 0,
+            y: 0,
+            width: textSize.width,
+            height: textSize.height
+        )
+        
+        preferredContentSize = CGSize(width: 200, height: 200)
+        
+        view.backgroundColor = .green
+        title = "Edit"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: NSLocalizedString("Save", comment: ""),
+            style: .plain,
+            target: self,
+            action: #selector(onMenuCancel)
+        )
+        navigationItem.rightBarButtonItem?.tintColor = Styles.Colors.Gray.light.color
+  
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+            title: Constants.Strings.cancel,
+            style: .plain,
+            target: self,
+            action: #selector(onMenuCancel)
+        )
+        navigationItem.leftBarButtonItem?.tintColor = Styles.Colors.Gray.light.color
+        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
+        
+    }
+    
+    @objc func onMenuCancel() {
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
