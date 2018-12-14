2a046e7dce36fcd3c0a961fff8ee300adf762360
Brian Litwin <brian.n.litwin@gmail.com>
2018-12-14 16:48:39 -0500

    search for labels on tap

---
+++ Classes/Repository/RepositorySearchBarQueryModel.swift
@@ -0,0 +1,13 @@
+//
+//  RepositorySearchBarQueryModel.swift
+//  Freetime
+//
+//  Created by B_Litwin on 12/12/18.
+//  Copyright © 2018 Ryan Nystrom. All rights reserved.
+//
+
+import UIKit
+
+class RepositorySearchBarQueryModel: UIVe {
+
+}
